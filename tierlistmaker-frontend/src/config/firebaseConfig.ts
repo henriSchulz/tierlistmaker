3f1d80a734abd4439de37e29b0f5686f28454e74
henrischulz <henri.schulz.bs@icloud.com>
2024-05-28 10:25:14 +0200

    bug fixes

--- tierlistmaker-frontend/src/config/firebaseConfig.ts
+++ tierlistmaker-frontend/src/config/firebaseConfig.ts
@@ -5,13 +5,7 @@ import {getFirestore} from "@firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC4U53YTFbVtLb_zMBzf4MpTVMTx9p9_I8",
-    authDomain: "tierlistmaker.org",
-    projectId: "sandbox-655bf",
-    storageBucket: "sandbox-655bf.appspot.com",
-    messagingSenderId: "666195911026",
-    appId: "1:666195911026:web:974744ed21487f38ae7deb",
-    measurementId: "G-FW347XQTXB"
+    //     FIREBASE CONFIGURATION
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,7 +16,6 @@ const firestore = getFirestore(app);
 
 
 const provider = new GoogleAuthProvider();
-
 
 
 export {auth, provider, storage, firestore}
